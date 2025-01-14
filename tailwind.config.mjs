af62513f94d5c6a4a6fdf013c0fb2fc1ef9b3fc7
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2025-01-13 23:22:45 -0500

    feat: Init and General Page Layout

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          "Noto Sans Variable",
+          "Inter Variable",
+          "Inter",
+          ...defaultTheme.fontFamily.sans,
+        ],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
